bb78d4eb172f104ff56646ebc164dad36aceeefa
edutekdi <kaafbizsolutions@gmail.com>
2025-06-06 17:57:43 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,15 @@
-const cacheName = 'Hadiah Untuk Ummi-pw'; // nama cache – boleh ubah jika ada versi baru
+const cacheName = ''Hadiah Untuk Ummi-pw';
+
 const assetsToCache = [
   './',
   './index.html',
-  './style.css',
-  './script.js',
   './manifest.json',
-  './audio/audio1.mp3',
-  './audio/audio2.mp3',
-  './audio/audio4.mp3',
-  './audio/audio5.mp3',
-  './audio/audio6.mp3',
-  './audio/audio7.mp3',
-  './audio/audio8.mp3',
-  './audio/audio8.mp3',
-  './audio/audio9.mp3',
-  './audio/audio10.mp3',
-  './audio/audio11(1).mp3',
-  './audio/audio12.mp3',
-  './audio/audio13.mp3',
-  './audio/audio14.mp3',
-  './audio/betul.mp3',
-  './audio/salah.mp3',
+  
+  // Ikon
+  './images_icon/icon192.png',
+  './images_icon/icon512.png',
+
+  // Gambar
   './images/coverdepan.png',
   './images/judul.png',
   './images/halaman3.png',
@@ -40,9 +29,26 @@ const assetsToCache = [
   './images/coverbelakang.png',
   './images/icon192.png',
   './images/icon512.png'
+
+  // Audio
+  ''./audio/audio1.mp3',
+  './audio/audio2.mp3',
+  './audio/audio4.mp3',
+  './audio/audio5.mp3',
+  './audio/audio6.mp3',
+  './audio/audio7.mp3',
+  './audio/audio8.mp3',
+  './audio/audio8.mp3',
+  './audio/audio9.mp3',
+  './audio/audio10.mp3',
+  './audio/audio11(1).mp3',
+  './audio/audio12.mp3',
+  './audio/audio13.mp3',
+  './audio/audio14.mp3',
+  './audio/betul.mp3',
+  './audio/salah.mp3',
 ];
 
-// Simpan fail semasa pemasangan awal
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -51,18 +57,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// Aktifkan service worker dan buang cache lama jika ada
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Baca dari cache jika offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
